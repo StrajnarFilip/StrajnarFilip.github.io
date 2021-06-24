634b9eba4fc2e34cbfac8e117d4792856d7576b3
Filip Strajnar <filip.strajnar@gmail.com>
2021-06-24 12:14:30 +0200

    Auto Message

--- main.js
+++ main.js
@@ -3,6 +3,5 @@ function main() {
     console.log("hi");
     document.getElementsByTagName("p")[0].onclick(()=>{console.log("clicked");})
 }
-console.log("Not ready");
 
 window.addEventListener("load", main)
