2cc4ffe4820881e054ede07c18ecb094988f7b7b
Filip Strajnar <filip.strajnar@gmail.com>
2021-06-24 18:01:19 +0200

    Auto Message

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ function main() {
         navigator.serviceWorker.register("/sw.js")
         console.log("Service Worker allowed");
     }
+    const reset = document.getElementById("reset_cache")
+    reset.addEventListener("click", () => {
+        const all_cache_names = await caches.keys();
+        all_cache_names.forEach(cache_name => {
+            caches.delete(cache_name);
+        });
+    })
 }
 
 window.addEventListener("load", main)
