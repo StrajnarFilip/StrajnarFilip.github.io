0dfdfcbe2b68bbb4d369cbd2cbb3e024d6c802ff
Filip Strajnar <filip.strajnar@gmail.com>
2021-06-26 14:08:32 +0200

    Auto Message

---
+++ main.ts
@@ -0,0 +1,26 @@
+function AES_encrypt(plaintext: string,password: string){
+    let ob=CryptoJS.AES.encrypt(plaintext,password);
+    console.log(`${ob.algorithm},${ob.blockSize},${ob.ciphertext},${ob.iv},${ob.key},${ob.mode},${ob.padding},${ob.salt}`);
+}
+function AES_decrypt(){
+
+}
+
+
+function main() {
+    console.log("hi");
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register("/sw.js")
+        console.log("Service Worker allowed");
+    }
+    const reset = document.getElementById("reset_cache")
+    reset?.addEventListener("click", async() => {
+        const all_cache_names = await caches.keys();
+        all_cache_names.forEach(cache_name => {
+            caches.delete(cache_name);
+        });
+    })
+
+}
+
+window.addEventListener("load", main)
