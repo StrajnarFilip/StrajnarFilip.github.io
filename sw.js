17407d555a1c5e7c392f362d32980923b7bbe38e
Filip Strajnar <filip.strajnar@gmail.com>
2021-06-27 01:02:52 +0200

    Auto Message

--- sw.js
+++ sw.js
@@ -73,5 +73,5 @@ async function ClearAllCache() {
     });
 }
 
-CacheResourcesOnInstallation(["index.html", "main.js", "sw.js","bootstrap.css","bootstrap.js","lock.png"])
+CacheResourcesOnInstallation(["index.html", "main.js", "sw.js","bootstrap.css","bootstrap.js","lock.png","manifest.json"])
 ActivateCacheFirst();
